252742af654b197abec72dfc36ab241e09470e9c
Amir H. Yeganemehr <ygnmehr@gmail.com>
2024-12-27 09:10:13 +0330

    Last Review and Fixes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Dedoc\Scramble\Scramble;
+use Illuminate\Support\Facades\Route;
 
 Scramble::registerUiRoute(path: 'docs/v1', api: 'v1');
 Scramble::registerJsonSpecificationRoute(path: 'docs/v1.json', api: 'v1');
+Route::redirect('/', 'docs/v1');
